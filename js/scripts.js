fcaeb27a4f2b0cf0bbcdad7989db18f609605c45
Jill Kuchman <jillkuchman@gmail.com>
2015-04-17 12:17:56 -0700

    Add crust selection to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var pizza = {
     getPrice: function() {
         return 10   + (this.topping * 2)
                     + (this.pizza_size * 3)
+                    + (this.crust * 3)
     }
 };
 
@@ -10,18 +11,19 @@ $(document).ready(function() {
         event.preventDefault();
 
         var toppings_multiplier = 0;
-
         $(':checkbox:checked.topping-selection').each(function() {
             toppings_multiplier += +this.value;
         });
 
-        var size_selection = parseInt($('.size-selection').val());
-        console.log(size_selection);
+        var size_selection = parseInt($('#size-selection').val());
+
+        var crust_selection = parseInt($('#crust-selection').val());
 
         var pizza_order = Object.create(pizza);
 
         pizza_order.topping = toppings_multiplier;
         pizza_order.pizza_size = size_selection;
+        pizza_order.crust = crust_selection;
 
         var pizza_price = pizza_order.getPrice();
 
