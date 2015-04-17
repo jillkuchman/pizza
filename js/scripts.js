d505303e455c95b0c232383d5183db49a65a4c34
Jill Kuchman <jillkuchman@gmail.com>
2015-04-17 10:48:30 -0700

    Replace getPrice method with formula, so each topping has a multiplier value

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 var Pizza = {
+    // getPrice: function() {
+    //     if (this.topping === 'cheese') {
+    //         return 10;
+    //     }
+    //     if (this.topping === 'pepperoni') {
+    //         return 15;
+    //     }
+    // }
     getPrice: function() {
-        if (this.topping === 'cheese') {
-            return 10;
-        }
-        if (this.topping === 'pepperoni') {
-            return 15;
-        }
+        return 10   + (this.topping * 2)
+                    
     }
 };
 
@@ -20,6 +24,8 @@ $(document).ready(function() {
         pizza_order.topping = topping_selection;
 
         var pizza_price = pizza_order.getPrice();
+
+        $('#pizza-price').show();
 
         $('#order-price').text(pizza_price);
     });
--- spec/specs.js
+++ spec/specs.js
@@ -2,13 +2,13 @@ describe('Pizza', function() {
     describe('getPrice', function() {
         it('will return a price of 10 for cheese', function() {
             var testPizza = Object.create(Pizza);
-            testPizza.topping = 'cheese';
+            testPizza.topping = 0;
             expect(testPizza.getPrice()).to.equal(10);
         });
-        it('will return a price of 15 for pepperoni', function() {
+        it('will return a price of 12 for pepperoni', function() {
             var testPizza = Object.create(Pizza);
-            testPizza.topping = 'pepperoni';
-            expect(testPizza.getPrice()).to.equal(15);
+            testPizza.topping = 1;
+            expect(testPizza.getPrice()).to.equal(12);
         });
     });
 });
