e8baaab5cde643ddfd23545b2f30058ef97f8bd2
Jill Kuchman <jillkuchman@gmail.com>
2015-04-17 12:31:55 -0700

    Update specs

--- spec/specs.js
+++ spec/specs.js
@@ -1,34 +1,53 @@
 describe('pizza', function() {
     describe('getPrice', function() {
-        it('will return a price of 10 for a topping with multiplier 0, size multiplier 0', function() {
+        it('will return a price of 10 for a topping with multiplier 0, size multiplier 0, crust multiplier 0', function() {
             var testPizza = Object.create(pizza);
             testPizza.topping = 0;
             testPizza.pizza_size = 0;
+            testPizza.crust = 0;
             expect(testPizza.getPrice()).to.equal(10);
         });
-        it('will return a price of 12 a topping with multiplier 1, size multiplier 0', function() {
+        it('will return a price of 12 a topping with multiplier 1, size multiplier 0, crust multiplier 0', function() {
             var testPizza = Object.create(pizza);
             testPizza.topping = 1;
             testPizza.pizza_size = 0;
+            testPizza.crust = 0;
             expect(testPizza.getPrice()).to.equal(12);
         });
-        it('will return a price of 13 for a topping with multiplier 0, size multiplier 1', function() {
+        it('will return a price of 12 a topping with multiplier -1, size multiplier 0, crust multiplier 0', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = -1;
+            testPizza.pizza_size = 0;
+            testPizza.crust = 0;
+            expect(testPizza.getPrice()).to.equal(8);
+        });
+        it('will return a price of 13 for a topping with multiplier 0, size multiplier 1, crust multiplier 0', function() {
             var testPizza = Object.create(pizza);
             testPizza.topping = 0;
             testPizza.pizza_size = 1;
+            testPizza.crust = 0;
             expect(testPizza.getPrice()).to.equal(13);
         });
-        it('will return a price of 15 for a topping with multiplier 1, size multiplier 1', function() {
+        it('will return a price of 15 for a topping with multiplier 1, size multiplier 1, crust multiplier 0', function() {
             var testPizza = Object.create(pizza);
             testPizza.topping = 1;
             testPizza.pizza_size = 1;
+            testPizza.crust = 0;
             expect(testPizza.getPrice()).to.equal(15);
         });
-        it('will return a price of 16 for a topping with multiplier 0, size multiplier 2', function() {
+        it('will return a price of 16 for a topping with multiplier 0, size multiplier 0, crust multiplier 1', function() {
             var testPizza = Object.create(pizza);
             testPizza.topping = 0;
-            testPizza.pizza_size = 2;
-            expect(testPizza.getPrice()).to.equal(16);
+            testPizza.pizza_size = 0;
+            testPizza.crust = 1;
+            expect(testPizza.getPrice()).to.equal(13);
+        });
+        it('will return a price of 16 for a topping with multiplier 1, size multiplier 1, crust multiplier 1', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = 1;
+            testPizza.pizza_size = 1;
+            testPizza.crust = 1;
+            expect(testPizza.getPrice()).to.equal(18);
         });
     });
 });
