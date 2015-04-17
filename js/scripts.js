de677026e4bcdb13a491f74e7ecd08a3c864fb45
Jill Kuchman <jillkuchman@gmail.com>
2015-04-17 12:10:09 -0700

    Add fully-functioning multiple topping selector and display price

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 var pizza = {
-    // getPrice: function() {
-    //     if (this.topping === 'cheese') {
-    //         return 10;
-    //     }
-    //     if (this.topping === 'pepperoni') {
-    //         return 15;
-    //     }
-    // }
     getPrice: function() {
         return 10   + (this.topping * 2)
                     + (this.pizza_size * 3)
@@ -18,19 +10,18 @@ $(document).ready(function() {
         event.preventDefault();
 
         var toppings_multiplier = 0;
-        // var topping_selection = $('#topping-selection').val();
+
         $(':checkbox:checked.topping-selection').each(function() {
             toppings_multiplier += +this.value;
-            console.log(toppings_multiplier);
         });
-        console.log('total: ' + toppings_multiplier);
 
-        var size_selection = $('#size-selection').val();
+        // var size_selection = $('#size-selection').val();
+        var size_selection = 0;
 
         var pizza_order = Object.create(pizza);
 
         pizza_order.topping = toppings_multiplier;
-        pizza_order.size_selection = size_selection;
+        pizza_order.pizza_size = size_selection;
 
         var pizza_price = pizza_order.getPrice();
 
