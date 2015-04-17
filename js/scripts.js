0a374fc5c01b83dcff179039b5a3274e717b7b9b
Jill Kuchman <jillkuchman@gmail.com>
2015-04-17 12:37:07 -0700

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// PROTOTYPE PIZZA OBJECT WITH getPrice METHOD
 var pizza = {
     getPrice: function() {
         return 10   + (this.topping * 2)
@@ -11,12 +12,15 @@ $(document).ready(function() {
         event.preventDefault();
 
         var toppings_multiplier = 0;
+        // ADDS CHECKED TOPPINGS TO TOPPINGS MULTIPLIER
         $(':checkbox:checked.topping-selection').each(function() {
             toppings_multiplier += +this.value;
         });
 
+        // SETS SIZE SELECTION TO USER INPUT
         var size_selection = parseInt($('#size-selection').val());
 
+        // SETS CRUST SELECTION TO USER INPUT
         var crust_selection = parseInt($('#crust-selection').val());
 
         var pizza_order = Object.create(pizza);
